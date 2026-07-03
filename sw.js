f5197cf5491ac57c6e159d20f4794931f4d2fdd6
Project PEXP <dev.pexp@gmail.com>
2026-07-03 12:16:50 +0100

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,25 +1,33 @@
-const CACHE_NAME = 'preco-certo-v1';
+const CACHE_NAME = "descontos-v1";
+
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://flaticon.com'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// Guarda os ficheiros no telemóvel durante a instalação
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Carrega os ficheiros do telemóvel em vez de ir buscar à internet
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
